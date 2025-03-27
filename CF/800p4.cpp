774d5037b50084262d7d80ff81ae59df96e1e84e
shuvokhan3 <shuvoj360@gmail.com>
2025-03-27 23:13:38 +0600

    27/3/2025

---
+++ CF/800p4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int sum1 = (s[0] - '0') + (s[1] - '0') + (s[2] - '0');
+        int sum2 = (s[3] - '0') + (s[4] - '0') + (s[5] - '0');
+        cout << (sum1 == sum2 ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
