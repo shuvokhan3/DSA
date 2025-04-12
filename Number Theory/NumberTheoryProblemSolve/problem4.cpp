8988742c12a7e351d991f2c226b23a82a9cf61df
shuvokhan3 <shuvoj360@gmail.com>
2025-04-12 12:10:48 +0600

    number theory

--- Number Theory/NumberTheoryProblemSolve/problem4.cpp
+++ Number Theory/NumberTheoryProblemSolve/problem4.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <map>
+#include <cmath>
 using namespace std;
 
-// Function to calculate GCD of two numbers
-int gcd(int a, int b) {
-    while (b) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+// Get prime factorization of a number
+map<int, int> getPrimeFactors(int num) {
+    map<int, int> factors;
+    
+    // Handle 2 as a special case
+    while (num % 2 == 0) {
+        factors[2]++;
+        num /= 2;
     }
-    return a;
+    
+    // Handle odd prime factors
+    for (int i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            factors[i]++;
+            num /= i;
+        }
+    }
+    
+    // If num is a prime number greater than 2
+    if (num > 1) {
+        factors[num]++;
+    }
+    
+    return factors;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    // For each element, keep only those prime factors with odd powers
+    vector<map<int, int>> oddFactors(n);
+    
+    for (int i = 0; i < n; i++) {
+        map<int, int> factors = getPrimeFactors(a[i]);
+        for (auto& [prime, power] : factors) {
+            if (power % 2 == 1) {
+                oddFactors[i][prime] = 1; // We only care if it's odd, not the exact count
+            }
+        }
+    }
+    
+    // If any element has odd prime factors, we can form a subsequence with that element
+    for (int i = 0; i < n; i++) {
+        if (!oddFactors[i].empty()) {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    
+    // If all elements are perfect squares (have no odd prime factors),
+    // then all subsequences will also be perfect squares
+    cout << "NO" << endl;
 }
 
 int main() {
@@ -21,25 +72,7 @@ int main() {
     cin >> t;
     
     while (t--) {
-        int n;
-        cin >> n;
-        
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        
-        // Check if there exists a pair with GCD <= 2
-        bool beautiful = false;
-        for (int i = 0; i < n && !beautiful; i++) {
-            for (int j = i + 1; j < n && !beautiful; j++) {
-                if (gcd(arr[i], arr[j]) <= 2) {
-                    beautiful = true;
-                }
-            }
-        }
-        
-        cout << (beautiful ? "Yes" : "No") << endl;
+        solve();
     }
     
     return 0;
