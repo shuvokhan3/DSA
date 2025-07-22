811567ccbc9cfc4c8bacb817e0ef732f98e4d902
shuvokhan3 <shuvoj360@gmail.com>
2025-07-22 11:41:48 +0600

    convert digit char to int value

---
+++ Number Theory/convert_string_to_int.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    char a = '6';
+    //to convert char to int 
+    int value = (a - '0') * 12;
+    cout<<value<<endl;
+}
