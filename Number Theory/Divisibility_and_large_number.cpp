3c75594aaa5e2fb6e3fc9466054d02d00e43eda9
shuvokhan3 <shuvoj360@gmail.com>
2025-07-22 11:57:08 +0600

    Is a is divisible by b or not , a maybe large number

---
+++ Number Theory/Divisibility_and_large_number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string a; int b;cin>>a>>b;
+    int ans = 0;
+
+    for(int i = 0 ;i < a.size() ;i++){
+        ans = (ans * 10LL % b + (a[i] - '0' )) % b;
+    }
+
+    //ans = a % b
+    if(ans == 0){
+        cout<<"a is divisible by b"<<endl;
+    }else{
+        cout<<"Not divisible"<<endl;
+    }
+    
+    return 0;
+
+}
