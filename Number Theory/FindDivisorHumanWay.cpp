1990cc6ac1ef1346147b85a3eb0e1620648b78f1
shuvokhan3 <shuvoj360@gmail.com>
2026-09-24 13:13:50 +0600

    number theory practice

--- Number Theory/FindDivisorHumanWay.cpp
+++ Number Theory/FindDivisorHumanWay.cpp
@@ -1,19 +1,43 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int32_t main (){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     int n;cin>>n;
+//     vector<int>v;
+//     for(int i = 1 ; i * i <= n ;i++){
+//         if(n % i == 0){
+//             v.push_back(i);
+//             v.push_back(n/i);
+//         }
+        
+//     }
+//     sort(v.begin(), v.end());
+//     for(int i = 0 ; i < v.size() ;i++){
+//         cout<<v[i]<<" ";
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-int32_t main (){
-    ios_base::sync_with_stdio(0);
+
+int main(){
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    int n;cin>>n;
+
+    int n; cin>>n;
     vector<int>v;
-    for(int i = 1 ; i * i <= n ;i++){
+
+    for(int i = 1; i <= sqrt(n); i++){
         if(n % i == 0){
             v.push_back(i);
             v.push_back(n/i);
         }
-        
     }
+
     sort(v.begin(), v.end());
-    for(int i = 0 ; i < v.size() ;i++){
+    for(int i = 0; i < v.size(); i++){
         cout<<v[i]<<" ";
     }
     return 0;
--- Number Theory/primeornot.cpp
+++ Number Theory/primeornot.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 
 bool is_prime(int n){
-    for(int i = 2; i * i <= n ; i++){
+
+    for(int i = 2 ; i <= sqrt(n); i++){
         if(n % i == 0){
             return false;
         }
     }
+
     return true;
 }
-int32_t main(){
+
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout<<is_prime(7)<<"\n";
+
+    cout<<is_prime(4)<<endl;
+
     return 0;
+
 }
